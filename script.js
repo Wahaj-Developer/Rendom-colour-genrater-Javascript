446d39ba9dc2a0cb74d5f07287d2fad4128016cb
Web-Code <ahmedwahaj317@gmail.com>
2025-05-14 00:18:11 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const get = () => {
+const random = Math.floor(Math.random() * 16777215);
+const randomcode = "#" + random.toString(16);
+document.body.style.backgroundColor = randomcode
+document.getElementById("color-code").innerText = randomcode
+}
 
+document.getElementById("btn").addEventListener(
+    "click",get
+)
+get()
